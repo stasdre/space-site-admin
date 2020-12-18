0b16028c600e5270edd3e9d9e369d6a7eb5eb98e
Stanislav Drehval <stasdre@gmail.com>
2020-12-18 10:27:06 +0200

    update form Services

--- src/components/Sections/Services/ServiceForm.js
+++ src/components/Sections/Services/ServiceForm.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Tabs, Input, Button, Space, Switch, Transfer } from 'antd';
+import {
+  Form,
+  Tabs,
+  Input,
+  InputNumber,
+  Button,
+  Space,
+  Switch,
+  Transfer,
+  Upload,
+} from 'antd';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import ReactQuill from 'react-quill';
@@ -9,6 +19,8 @@ import { seviceCreateRequest } from '../../../modules/Service';
 import 'react-quill/dist/quill.snow.css';
 
 const { TabPane } = Tabs;
+const { TextArea } = Input;
+
 const operations = (
   <>
     <Link to="/services">
@@ -22,13 +34,15 @@ const operations = (
 
 const ServiceForm = ({ workGetAllRequest, seviceCreateRequest, works }) => {
   const [targetKeys, setTargetKeys] = useState([]);
+  const [imgPrev, setImgPrev] = useState('');
 
   useEffect(() => {
     workGetAllRequest();
   }, []);
 
   const handleSubmit = (values) => {
-    seviceCreateRequest(values);
+    console.log('Data!!!', values);
+    //seviceCreateRequest(values);
   };
 
   const workList = works.map((item) => ({
@@ -40,24 +54,94 @@ const ServiceForm = ({ workGetAllRequest, seviceCreateRequest, works }) => {
     setTargetKeys(nextTargetKeys);
   };
 
+  const handleVideoPreviewChange = (file) => {
+    console.log('File!', file);
+    return;
+  };
+
   return (
-    <Form initialValues={{ active: true }} onFinish={handleSubmit}>
+    <Form
+      labelCol={{ span: 2 }}
+      wrapperCol={{ span: 14 }}
+      initialValues={{ active: true }}
+      layout="horizontal"
+      onFinish={handleSubmit}
+    >
       <Tabs defaultActiveKey="1" tabBarExtraContent={operations}>
-        <TabPane tab="Main" key="1">
-          <Form.Item name="active">
-            <Switch
-              checkedChildren="Активна"
-              unCheckedChildren="Не активна"
-              defaultChecked
-            />
+        <TabPane tab="Meta" key="1">
+          <Form.Item
+            name="meta_title[en]"
+            label="Meta title"
+            rules={[{ required: true, message: 'Title обязателен' }]}
+          >
+            <Input placeholder="Meta title" />
+          </Form.Item>
+          <Form.Item
+            name="meta_desc"
+            label="Meta description"
+            rules={[{ required: true, message: 'Description обязателен' }]}
+          >
+            <TextArea rows={4} placeholder="Meta description" />
+          </Form.Item>
+          <Form.Item
+            name="h1"
+            label="Заголовок h1"
+            rules={[{ required: true, message: 'h1 обязателен' }]}
+          >
+            <Input placeholder="Заголовок h1" />
+          </Form.Item>
+        </TabPane>
+        <TabPane tab="Блок с видео" key="2">
+          <Form.Item name="h2" label="Заголовок h2">
+            <Input placeholder="Заголовок h2" />
+          </Form.Item>
+          <Form.Item name="video_name" label="Заголовок видео">
+            <Input placeholder="Заголовок видео" />
+          </Form.Item>
+          <Form.Item name="video_prev" label="Превью" valuePropName="">
+            <Upload
+              listType="picture-card"
+              accept="image/jpeg,image/jpg,image/png"
+              beforeUpload={false}
+              showUploadList={false}
+              onChange={handleVideoPreviewChange}
+            >
+              {imgPrev ? (
+                <img src={imgPrev} alt="avatar" style={{ width: '100%' }} />
+              ) : (
+                <PlusOutlined />
+              )}
+            </Upload>
+          </Form.Item>
+          <Form.Item name="video_url" label="Ссылка на видео">
+            <Input placeholder="Ссылка на видео" />
+          </Form.Item>
+          <Form.Item name="description" label="Описание">
+            <ReactQuill theme="snow" />
+          </Form.Item>
+        </TabPane>
+        <TabPane tab="Main" key="3">
+          <Form.Item name="active" valuePropName="checked">
+            <Switch checkedChildren="Активна" unCheckedChildren="Не активна" />
+          </Form.Item>
+          <Form.Item name="main_title" label="Заголовок">
+            <Input placeholder="Заголовок" />
           </Form.Item>
           <Form.Item
             name="name"
+            label="Название"
             rules={[{ required: true, message: 'Название обязательно' }]}
           >
             <Input placeholder="Название услуги" />
           </Form.Item>
-          <Form.Item name="works">
+          <Form.Item
+            name="url"
+            label="URL"
+            rules={[{ required: true, message: 'URL обязателен' }]}
+          >
+            <Input placeholder="URL" />
+          </Form.Item>
+          <Form.Item label="Работы" name="works">
             <Transfer
               dataSource={workList}
               render={(item) => item.title}
@@ -67,15 +151,26 @@ const ServiceForm = ({ workGetAllRequest, seviceCreateRequest, works }) => {
               titles={['Все работы', 'Привязанные']}
             />
           </Form.Item>
-          <Form.Item name="reviews">
-            <Transfer titles={['Все отзывы', 'Привязанные']} />
+          <Form.Item label="Отзывы" name="reviews">
+            <Transfer oneWay titles={['Все отзывы', 'Привязанные']} />
           </Form.Item>
         </TabPane>
-        <TabPane tab="Описание" key="2">
-          <Form.Item name="desc">
+        <TabPane tab="Описание" key="4">
+          <Form.Item
+            name="desc_hash"
+            label="Hash"
+            rules={[{ required: true, message: 'Hash обязателен' }]}
+          >
+            <Input placeholder="Hash" prefix="#" />
+          </Form.Item>
+          <Form.Item
+            name="desc"
+            label="Текст описания"
+            rules={[{ required: true, message: 'Текст обязателен' }]}
+          >
             <ReactQuill theme="snow" />
           </Form.Item>
-          <Form.List name="advantage">
+          <Form.List name="advantage" label="">
             {(fields, { add, remove }) => (
               <>
                 {fields.map((field) => (
@@ -88,7 +183,7 @@ const ServiceForm = ({ workGetAllRequest, seviceCreateRequest, works }) => {
                       {...field}
                       name={[field.name, 'title']}
                       fieldKey={[field.fieldKey, 'title']}
-                      style={{ width: '300px' }}
+                      wrapperCol={{ span: 24 }}
                     >
                       <Input placeholder="Название" />
                     </Form.Item>
@@ -104,53 +199,90 @@ const ServiceForm = ({ workGetAllRequest, seviceCreateRequest, works }) => {
             )}
           </Form.List>
         </TabPane>
-        <TabPane tab="Стоимость" key="3">
-          <Form.List name="prices">
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map((field) => (
-                  <Space
-                    key={field.key}
-                    style={{ display: 'flex', marginBottom: 8 }}
-                    align="baseline"
-                  >
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'title']}
-                      fieldKey={[field.fieldKey, 'title']}
-                      rules={[{ required: true, message: 'Заголовок обязателен' }]}
+        <TabPane tab="Цены" key="5">
+          <Form.Item
+            name="price_hash"
+            label="Hash"
+            rules={[{ required: true, message: 'Hash обязателен' }]}
+          >
+            <Input placeholder="Hash" prefix="#" />
+          </Form.Item>
+          <Form.List name="price">
+            {(fields, { add, remove }) => {
+              return (
+                <>
+                  {fields.map((field) => (
+                    <Space
+                      key={field.key}
+                      style={{ display: 'flex', marginBottom: 8 }}
+                      align="baseline"
                     >
-                      <Input placeholder="Заголовок" />
-                    </Form.Item>
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'price']}
-                      fieldKey={[field.fieldKey, 'price']}
-                      rules={[{ required: true, message: 'Цена обязательна' }]}
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'title']}
+                        fieldKey={[field.fieldKey, 'title']}
+                        rules={[{ required: true, message: 'Название обязательно' }]}
+                        wrapperCol={{ span: 24 }}
+                      >
+                        <Input placeholder="Название" />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'from']}
+                        fieldKey={[field.fieldKey, 'from']}
+                        valuePropName="checked"
+                      >
+                        <Switch checkedChildren="От" unCheckedChildren="От" />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'price']}
+                        fieldKey={[field.fieldKey, 'price']}
+                        rules={[{ required: true, message: 'Неверная цена' }]}
+                        wrapperCol={{ span: 24 }}
+                      >
+                        <InputNumber placeholder="Цена" prefix="$" />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'promo']}
+                        fieldKey={[field.fieldKey, 'promo']}
+                        wrapperCol={{ span: 24 }}
+                      >
+                        <Input placeholder="Промо" />
+                      </Form.Item>
+
+                      <MinusCircleOutlined onClick={() => remove(field.name)} />
+                    </Space>
+                  ))}
+                  <Form.Item>
+                    <Button
+                      type="dashed"
+                      disabled={fields.length === 3}
+                      onClick={() => add()}
+                      icon={<PlusOutlined />}
                     >
-                      <Input placeholder="Цена" />
-                    </Form.Item>
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'additional']}
-                      fieldKey={[field.fieldKey, 'additional']}
-                    >
-                      <Input placeholder="Дополнительно" />
-                    </Form.Item>
-                    <MinusCircleOutlined onClick={() => remove(field.name)} />
-                  </Space>
-                ))}
-                <Form.Item>
-                  <Button type="dashed" onClick={() => add()} icon={<PlusOutlined />}>
-                    Добавить тарифный план
-                  </Button>
-                </Form.Item>
-              </>
-            )}
+                      Добавить цену
+                    </Button>
+                  </Form.Item>
+                </>
+              );
+            }}
           </Form.List>
         </TabPane>
-        <TabPane tab="Подробнее" key="4">
-          <Form.Item name="more">
+        <TabPane tab="Подробнее" key="6">
+          <Form.Item
+            name="more_hash"
+            label="Hash"
+            rules={[{ required: true, message: 'Hash обязателен' }]}
+          >
+            <Input placeholder="Hash" prefix="#" />
+          </Form.Item>
+          <Form.Item
+            name="more"
+            label="Текст"
+            rules={[{ required: true, message: 'Текст обязателен' }]}
+          >
             <ReactQuill theme="snow" />
           </Form.Item>
         </TabPane>
--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,3 +1,4 @@
 export { Services } from './Services';
 export { Works } from './Works';
 export { Reviews } from './Reviews';
+export { Prices } from './Prices';
